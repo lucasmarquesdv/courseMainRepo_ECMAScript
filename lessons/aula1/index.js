d95ca8405d3e2f82b33715b6445417b0f7010349
lucasmarquesdv <lucasmarquesskt2004@gmail.com>
2024-01-20 01:55:14 -0300

    first commit

---
+++ lessons/aula1/index.js
@@ -0,0 +1,13 @@
+// Variáveis
+const nomePessoa = `Lucas Marques`;
+const idadePessoa = console.log(`Quantos anos você tem?`);
+const cidadePessoa = console.log(`['SP','MG','BA','CE','RS','DF','PI','SC','MA','AM','AC','GO','TO','RJ','RN','BH','MT','PR','RR']`);
+console.log(`Olá, ${nomePessoa}, você é um querido`);
+console.log(`A sua idade recente é de: ${idadePessoa}`);
+console.log(`A cidade da pessoa é: ${cidadePessoa}`);
+
+/*
+Depois voltarei aqui em breve para arrumar aquela array. Eu vou continuar fazendo o curso e depois volto aqui para terminar.
+
+OBS: Eu já tinha começado o curso antes, mas mudei de sistema operacional e optei por não fazer o backup, então perdi todos os meus saves e, juntamente com eles, os meus diretórios do curso.
+*/
