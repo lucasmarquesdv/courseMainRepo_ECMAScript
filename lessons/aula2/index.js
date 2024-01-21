7e250f291773353f705f5a51102b1b6c1c356f79
lucasmarquesdv <lucasmarquesskt2004@gmail.com>
2024-01-21 15:10:23 -0300

    update-02_21-1-24_LTS

---
+++ lessons/aula2/index.js
@@ -0,0 +1,9 @@
+// Introdução sobre functions
+
+const namePeople = console.log('NAME PEOPLE');
+const agePeople = console.log('Your age');
+let media = 5.0;
+let infoTasks = true;
+
+console.log(`Olá, ${namePeople}, como estás? A sua idade atual é ${agePeople}.`);
+console.log(`A média de corte da prova é de: ${media}`);
---
+++ lessons/aula4/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.54789;
+let num2 = Math.floor(num1); //Arredonda para baixo
+
+console.log(num2);
+
+let num3 = 9.54989;
+let num4 = Math.ceil(num1); //Arredonda para cima
+
+console.log(num4);
