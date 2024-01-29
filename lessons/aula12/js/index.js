40195759585535f349ad7669e80872db8172da95
Lucas Marques <lucasmarquesskt2004@gmail.com>
2024-01-29 00:06:51 -0300

    imgoingthroughtchanges

--- lessons/aula12/js/index.js
+++ lessons/aula12/js/index.js
@@ -5,3 +5,18 @@ Operadores lógicos
 ! -> NOT -> NÃO
 */
 console.log(true && true);
+
+const expressaoAnd = true && true && true;
+const expressaoOr = false || false || false;
+
+const user = 'Lucas '; //form username usuário digitou
+const password = '123456'; //form user password usuário digitou
+
+const vaiLogar = user === 'Lucas' && password === '123456';
+console.log(vaiLogar);
+
+//Operador NOT
+
+console.log(!true); //o ! faz com que true vire ao contrário e transforme em false. Se você colocasse um valor false ali, retornaria true.
+
+console.log(!!true); //ele negará uma vez o true, o true passa a ser falso. Se ele negar a outra vez, o true passa a ser true novamente.
